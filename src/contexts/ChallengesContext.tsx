b93af1fb6e97fdb59d0fcab51d8050e5fa25bffb
João Victor <j2003.victir@gmail.com>
2021-02-24 15:11:13 -0300

    Finish challenges business rule

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,12 +1,21 @@
 import { createContext, ReactNode, useState } from 'react';
+import challenges from '../../challenges.json';
 
+interface Challenge {
+	type: 'body' | 'eye';
+	description: string;
+	amount: number;
+}
 
 interface ChallengesContextData {
 	level: number;
 	currentExperience: number;
 	challengesCompleted: number;
+	activeChallenge: Challenge;
+	experienceToNextLevel: number;
 	levelUp: () => void;
 	startNewChallenge: () => void;
+	resetChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -20,13 +29,24 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 	const [currentExperience, setCurrentExperience] = useState(0);
 	const [challengesCompleted, setChallengesCompleted] = useState(0);
 
+	const [activeChallenge, setActiveChallenge] = useState(null);
+
+	const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+
 
 	function levelUp() {
 		setLevel(level + 1);
 	}
 
 	function startNewChallenge() {
-		console.log('new Challenge');
+		const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+		const challenge = challenges[randomChallengeIndex];
+
+		setActiveChallenge(challenge);
+	}
+
+	function resetChallenge() {
+		setActiveChallenge(null);
 	}
 
 	return (
@@ -37,6 +57,9 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 				levelUp,
 				currentExperience,
 				challengesCompleted,
+				activeChallenge,
+				resetChallenge,
+				experienceToNextLevel,
 			}}
 		>
 			{children}
